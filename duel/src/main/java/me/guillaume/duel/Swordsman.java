549b7fb62c48d4f4b0673e24b824c933c16f5dfd
crapea <crapea@EN1310100.endava.net>
2020-03-13 09:43:47 +0200

    fixed 2nd unit test

--- duel/src/main/java/me/guillaume/duel/Swordsman.java
+++ duel/src/main/java/me/guillaume/duel/Swordsman.java
@@ -3,8 +3,10 @@ package me.guillaume.duel;
 public class Swordsman {
 
 	public static final int WEAPON_DAMAGE = 5;
+	public static final String BUCKLER = "buckler";
 
 	private int hitPoints = 100;
+	private boolean hasBuckler = false;
 
 	public Swordsman() {
 	}
@@ -16,20 +18,55 @@ public class Swordsman {
 	}
 
 	public void engage(Viking viking) {
+		int swordsmansBucklerHits = 0;
+		int swordsmansBucklerLife = 3;
+		int vikingsBucklerHits = 0;
+
 		while (this.hitPoints > 0 && viking.hitPoints() > 0) {
-			viking.setHitPoints(viking.hitPoints() - Swordsman.WEAPON_DAMAGE);
-			this.hitPoints -= Viking.WEAPON_DAMAGE;
+
+			if (!viking.hasBuckler()) {
+				viking.setHitPoints(viking.hitPoints() - Swordsman.WEAPON_DAMAGE);
+			} else {
+				vikingsBucklerHits++;
+				if (vikingsBucklerHits % 2 != 0) {
+					viking.setHitPoints(viking.hitPoints() - Swordsman.WEAPON_DAMAGE);
+				}
+			}
+
+			if (!this.hasBuckler()) {
+				this.hitPoints -= Viking.WEAPON_DAMAGE;
+			} else {
+				swordsmansBucklerHits++;
+				if (swordsmansBucklerHits % 2 != 0 || swordsmansBucklerLife == 0) {
+					this.hitPoints -= Viking.WEAPON_DAMAGE;
+				} else {
+					swordsmansBucklerLife--;
+				}
+			}
 		}
+
 		this.hitPoints = this.hitPoints < 0 ? 0 : this.hitPoints;
 		viking.setHitPoints(viking.hitPoints() < 0 ? 0 : viking.hitPoints());
+
 	}
 
-	public Swordsman equip(String string) {
-		return null;
+	public Swordsman equip(String item) {
+		if (BUCKLER.equals(item)) {
+			this.setBuckler(true);
+		}
+		return this;
 	}
 
 	public int hitPoints() {
-		return 0;
+		return hitPoints;
+	}
+
+	public boolean hasBuckler() {
+		return hasBuckler;
+	}
+
+	public void setBuckler(boolean hasBuckler) {
+		this.hasBuckler = hasBuckler;
 	}
 
 }
--- duel/src/main/java/me/guillaume/duel/Viking.java
+++ duel/src/main/java/me/guillaume/duel/Viking.java
@@ -3,11 +3,16 @@ package me.guillaume.duel;
 public class Viking {
 
 	public static final int WEAPON_DAMAGE = 6;
+	public static final String BUCKLER = "buckler";
 
 	private int hitPoints = 120;
+	private boolean hasBuckler = false;
 
-	public Viking equip(String string) {
-		return null;
+	public Viking equip(String item) {
+		if (BUCKLER.equals(item)) {
+			this.setBuckler(true);
+		}
+		return this;
 	}
 
 	public int hitPoints() {
@@ -16,6 +21,14 @@ public class Viking {
 
 	public void setHitPoints(int hitPoints) {
 		this.hitPoints = hitPoints;
+	}
+
+	public boolean hasBuckler() {
+		return hasBuckler;
+	}
+
+	public void setBuckler(boolean hasBuckler) {
+		this.hasBuckler = hasBuckler;
 	}
 
 }
